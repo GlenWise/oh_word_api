b8014101bc276ade82f399e158d1f7a7d8b22b9a
Glen Wise <gwise@andrew.cmu.edu>
2016-12-01 22:17:09 -0500

    Successfully transfered to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161130042555) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "songs", force: :cascade do |t|
     t.string   "title"
     t.string   "artist"
